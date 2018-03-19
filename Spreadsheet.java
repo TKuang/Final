42e755b098c8d7b40da55b2ea810bb3ba36b134b
TKuang <31107156+TKuang@users.noreply.github.com>
2018-03-19 08:36:10 -0700

    Update Spreadsheet.java

--- Spreadsheet.java
+++ Spreadsheet.java
@@ -13,36 +13,76 @@ public class Spreadsheet implements Grid
 	@Override
 	public String processCommand(String command)
 	{
-		// TODO Auto-generated method stub
-		return "";
+		String[] userInput = command.split(" ", 3);
+		if((userInput[0].toLowerCase().equals("clear")) && userInput.length == 2) {
+			clear(userInput[1]);
+		}else if(userInput[0].toLowerCase().equals("clear")) {
+			clear();
+		}else if(userInput.length == 1 && !userInput[0].equals("clear")) {
+			SpreadsheetLocation loc = new SpreadsheetLocation(userInput[0]);
+			return getCell(loc).fullCellText();
+		}else if(userInput.length == 3){
+			SpreadsheetLocation loc = new SpreadsheetLocation(userInput[0]);
+			grid[loc.getRow()][loc.getCol()] = new TextCell(userInput[2]);
+			return getGridText();
+		}else {
+			return "Invalid command";
+		}
+		
+		return getGridText();
 	}
-
 	@Override
 	public int getRows()
 	{
-		// TODO Auto-generated method stub
 		return row;
 	}
 
 	@Override
 	public int getCols()
 	{
-		// TODO Auto-generated method stub
 		return col;
 	}
 
 	@Override
 	public Cell getCell(Location loc)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return grid[loc.getRow()][loc.getCol()];
 	}
 
 	@Override
 	public String getGridText()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String sheet = "";
+		sheet += "   ";
+		for(char i = 'A'; i <= 'L'; i++) {
+			sheet += ("|" + i + "         ");
+		}
+		sheet += "|\n";
+		for(int i = 0; i < grid.length; i++) {
+			if(i < 9) {
+				sheet += i + 1 + "  ";
+				
+			}else {
+				sheet += i + 1 + " ";
+			}
+			for(int j = 0; j < grid[i].length; j++) {
+				sheet += "|" + grid[i][j].abbreviatedCellText();
+			}
+			sheet += "|\n";
+		}
+		return sheet;
 	}
-
+	
+	public void clear(String cellName) {
+		SpreadsheetLocation loc = new SpreadsheetLocation(cellName);
+		grid[loc.getRow()][loc.getCol()] = new EmptyCell();
+	}
+	
+	public void clear() {
+		for(int i = 0; i < grid.length; i++) {
+			for(int j = 0; j < grid[i].length; j++) {
+				grid[i][j] = new EmptyCell();
+			}
+		}
+	}
 }
