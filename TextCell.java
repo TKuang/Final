39025b3d635dbac6beb3c50e70b0ce0e94b7575a
TKuang <31107156+TKuang@users.noreply.github.com>
2018-03-19 08:34:09 -0700

    Update TextCell.java

--- TextCell.java
+++ TextCell.java
@@ -1,1 +1,24 @@
-public Textcell implements Cell {
+public class TextCell implements Cell{
+	private String entry;
+	public TextCell(String entry) {
+		this.entry = entry;
+	}
+	
+	
+	public String abbreviatedCellText() {
+		String abbr = entry.substring(1, entry.length()-1);
+		if(abbr.length() < 10) {
+			while(abbr.length() < 10) {
+				abbr += " ";
+			}
+			return abbr;
+		}else {
+			return abbr.substring(0, 10);
+		}
+		
+	}
+	
+	public String fullCellText() {
+		return entry;
+	}
+}
