bc38dde62e6ccf5a4a36217fe5f3b6939b53a7f2
TKuang <31107156+TKuang@users.noreply.github.com>
2018-03-23 08:28:39 -0700

    Update FormulaCell.java

--- FormulaCell.java
+++ FormulaCell.java
@@ -1,1 +1,18 @@
-
+public class FormulaCell extends RealCell{
+	
+  public FormulaCell(String entry) {
+		super(entry);
+	}
+	
+	public String abbreviatedCellText() {
+		return "";
+	}
+	
+	public String fullCellText() {
+		return super.getRealCell();
+	}
+	
+	public double getDoubleValue() {
+		return 0;
+	}
+}
