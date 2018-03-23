66b757c88ff591186a78f7c55b2077efef44e1e1
TKuang <31107156+TKuang@users.noreply.github.com>
2018-03-23 08:22:26 -0700

    Update ValueCell.java

--- ValueCell.java
+++ ValueCell.java
@@ -1,1 +1,26 @@
 public ValueCell extends RealCell{
+  
+  	public ValueCell(String entry) {
+		  super(entry);
+	}
+	public String abbreviatedCellText() {
+		String text = Double.toString(getDoubleValue());
+		if(text.length() < 10) {
+			if(!text.contains(".")) {    //checks if its a whole number 
+				text += ".0";
+			}
+			return super.fillSpaces(text);
+		}else {	
+			return text.substring(0,10);
+		}
+	}
+	
+	public String fullCellText() {
+		return getRealCell();
+	}
+	
+	public double getDoubleValue() {
+		return Double.parseDouble(getRealCell());
+	}
+	
+}
