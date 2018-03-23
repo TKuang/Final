fe4c3dcaab65fd9abb5b26d8ea8f177f72c6a816
TKuang <31107156+TKuang@users.noreply.github.com>
2018-03-23 08:27:13 -0700

    Update PercentCell.java

--- PercentCell.java
+++ PercentCell.java
@@ -1,1 +1,21 @@
 public PercentCell extends RealCell {
+  
+	public PercentCell(String entry){
+		super(entry);
+	}
+	
+	public String abbreviatedCellText() {
+		String abbreviated = super.getRealCell().substring(0, super.getRealCell().indexOf('.')) + "%";
+		return super.fillSpaces(abbreviated);
+	}
+	
+	public String fullCellText() {
+		return (getDoubleValue() * 0.01) + "";
+	}
+	
+	public double getDoubleValue() {
+		double percent = Double.parseDouble(super.getRealCell().substring(0, super.getRealCell().length()-1));
+		return percent;
+	}
+		
+}
