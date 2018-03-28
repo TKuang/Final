518e3eea6b815e1bb5c20edb63c45ea916ae4341
TKuang <31107156+TKuang@users.noreply.github.com>
2018-03-28 10:47:37 -0700

    Update RealCell.java

--- RealCell.java
+++ RealCell.java
@@ -1,21 +1,25 @@
+package textExcel;
+
 public abstract class RealCell implements Cell{
 	private String input;
-	
 	public RealCell(String input) {
 		this.input = input;
 	}
 	
 	public abstract double getDoubleValue();
 	
+	
 	public abstract String abbreviatedCellText(); 
+		
+	
 	
 	public abstract String fullCellText();
 	
-	public String spaces(String value) {
-		while(value.length() < 10) {
-			value += " ";
+	public String spaces(String input) {
+		while(input.length() < 10) {
+			input += " ";
 		}
-		return value;
+		return input;
 	}
 	
 	public String getRealCell() {
