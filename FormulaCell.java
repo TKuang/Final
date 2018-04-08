3a3c4dd1f3b627c4d922f23226a24b1c1021122e
TKuang <31107156+TKuang@users.noreply.github.com>
2018-04-08 14:06:10 -0700

    Update FormulaCell.java

--- FormulaCell.java
+++ FormulaCell.java
@@ -4,15 +4,52 @@ public class FormulaCell extends RealCell{
 		super(entry);
 	}
 	
-	public String abbreviatedCellText() {
-		return "";
+  public String abbreviatedCellText() {
+		String finalString = getDoubleValue() + "";
+		if(finalString.length() < 10) {
+			return fillSpaces(finalString);
+		}else {
+			return finalString.substring(0, 10);
+		}
+		
 	}
-	
+
 	public String fullCellText() {
 		return super.getRealCell();
 	}
 	
 	public double getDoubleValue() {
-		return 0;
+		String modified = getRealCell().substring(2, getRealCell().length()-2);
+		String[] arr = modified.split(" ");
+		double result = 0;
+		if(arr.length == 1) {
+			result = Double.parseDouble(arr[0]);
+		}else {
+			if(arr[1].equals("+")) {
+				result = Double.parseDouble(arr[0]) + Double.parseDouble(arr[2]);
+			}else if(arr[1].equals("-")) {
+				result = Double.parseDouble(arr[0]) - Double.parseDouble(arr[2]);
+			}else if(arr[1].equals("*")) {
+				result = Double.parseDouble(arr[0]) * Double.parseDouble(arr[2]);
+			}else if(arr[1].equals("/")){
+				result = Double.parseDouble(arr[0]) / Double.parseDouble(arr[2]);
+			}
+		}
+		
+		if(arr.length == 1) {
+			result = Double.parseDouble(arr[0]);
+		}else{
+			for(int i = 2; i < arr.length; i++) {
+				if(arr[i].equals("+")){
+					result += Double.parseDouble(arr[i+1]);
+				}else if(arr[i].equals("-")) {
+					result -= Double.parseDouble(arr[i+1]);
+				}else if(arr[i].equals("*")){
+					result *= Double.parseDouble(arr[i+1]);
+				}else if(arr[i].equals("/")){
+					result /= Double.parseDouble(arr[i+1]);
+				}
+			}
+		}
+		return result;
 	}
-}
