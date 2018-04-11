dba54df6b939246a937dd65d5ed19630dab0b3e7
TKuang <31107156+TKuang@users.noreply.github.com>
2018-04-11 11:30:28 -0700

    Update FormulaCell.java

--- FormulaCell.java
+++ FormulaCell.java
@@ -1,15 +1,15 @@
+package textExcel;	
+
 public class FormulaCell extends RealCell{
-	
-  public FormulaCell(String input) {
+	public FormulaCell(String input) {
 		super(input);
 	}
 	
-  public String abbreviatedCellText() {
+	public String abbreviatedCellText() {
 		String display = getDoubleValue() + "";
 		if(display.length() < 10) {
-			return fillSpaces(display);
-		}
-	  	else {
+			return spaces(display);
+		}else {
 			return display.substring(0, 10);
 		}
 		
@@ -22,29 +22,23 @@ public class FormulaCell extends RealCell{
 	public double getDoubleValue() {
 		String formula = getRealCell().substring(2, getRealCell().length()-2);
 		String[] arr = formula.split(" ");
-		double result = 0;
-		if(arr.length == 1) {
-			result = Double.parseDouble(arr[0]);
-		}
-		else {
-			for(int i = 2; i < arr.length; i++) {
-				if(arr[1].equals("+")) {
-					result = Double.parseDouble(arr[0]) + Double.parseDouble(arr[2]) 
+		double result = Double.parseDouble(arr[0]);
+		if(arr.length != 1) {
+			for(int i = 1; i < arr.length; i++) {
+				if(arr[i].equals("+")){
 					result += Double.parseDouble(arr[i+1]);
 				}
-				else if(arr[1].equals("-")) {
-					result = Double.parseDouble(arr[0]) - Double.parseDouble(arr[2]) 
+				else if(arr[i].equals("-")) {
 					result -= Double.parseDouble(arr[i+1]);
 				}
-				else if(arr[1].equals("*")) {
-					result = Double.parseDouble(arr[0]) * Double.parseDouble(arr[2]) 
+				else if(arr[i].equals("*")){
 					result *= Double.parseDouble(arr[i+1]);
 				}
-				else if(arr[1].equals("/")){
-					result = Double.parseDouble(arr[0]) / Double.parseDouble(arr[2]) 
+				else if(arr[i].equals("/")){
 					result /= Double.parseDouble(arr[i+1]);
 				}
 			}
 		}
 		return result;
 	}
+}
