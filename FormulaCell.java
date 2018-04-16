06daf593e56ed443b7f1ad0f942d636c2a8e8777
TKuang <31107156+TKuang@users.noreply.github.com>
2018-04-15 22:56:04 -0700

    Update FormulaCell.java

--- FormulaCell.java
+++ FormulaCell.java
@@ -22,30 +22,61 @@ public class FormulaCell extends RealCell{
 	public double getDoubleValue() {
 		String formula = getRealCell().substring(2, getRealCell().length()-2);
 		String[] arr = formula.split(" ");
-		for (int i = 0; i < arr.length; i++){
-			if (arr[i] instanceof Cell){
+		double result = 0;
+		if(arr[0].toLowerCase().equals("avg")){
+			result = avg(arr[]);
+		}
+		else if(arr[0].toLowerCase().equals("sum")){
+			result = avg(arr[]);
+		}
+		else {
+			for (int i = 0; i < arr.length; i++){
 				SpreadsheetLocation location = new SpreadsheetLocation(arr[i]);
-				RealCell stored = getCell(location).fullCellText();
-				arr[i] = RealCell.getDoubleValue(stored);
-			}
-		}
-		double result = Double.parseDouble(arr[0]);
-		if(arr.length != 1) {
-			for(int i = 1; i < arr.length; i++) {
-				if(arr[i].equals("+")){
-					result += Double.parseDouble(arr[i+1]);
-				}
-				else if(arr[i].equals("-")) {
-					result -= Double.parseDouble(arr[i+1]);
-				}
-				else if(arr[i].equals("*")){
-					result *= Double.parseDouble(arr[i+1]);
-				}
-				else if(arr[i].equals("/")){
-					result /= Double.parseDouble(arr[i+1]);
+				if (grid.getCell(location) instanceof Cell){
+					RealCell stored = (RealCell) grid.getCell(location);
+					arr[i] = stored.getDoubleValue();
 				}
 			}
+			result = Double.parseDouble(arr[0]);
+			if(arr.length != 1) {
+				for(int i = 1; i < arr.length; i++) {
+					if(arr[i].equals("+")){
+						result += Double.parseDouble(arr[i+1]);
+					}
+					else if(arr[i].equals("-")) {
+						result -= Double.parseDouble(arr[i+1]);
+					}
+					else if(arr[i].equals("*")){
+						result *= Double.parseDouble(arr[i+1]);
+					}
+					else if(arr[i].equals("/")){
+						result /= Double.parseDouble(arr[i+1]);
+				}	
+			}
 		}
+		
 		return result;
 	}
+		
+		public double sum(int[] input){
+			String[] arr = imput.split("-");
+			int startrow = Integer.parseInt(arr[0].substring(1));
+			int finishrow = Integer.parseInt(arr[1].substring(1));
+			char startcol = arr[0].charAt(0);
+			char finishcol = arr[0].chartAt(0);
+			double sum = 0;
+			for (char i = startcol; i <= finishcol; i++){
+				for(int j = startrow; j <= finishrow; j++){
+					SpreadsheetLocation location = new SpreadsheetLocation("" + i + j);
+					if (grid.getCell(location) instanceof Cell){
+						RealCell stored = (RealCell) grid.getCell(location);
+						sum += stored.getDoubleValue();
+					}
+				}
+			}
+			return sum;
+		}
+					
+			
+			
 }
