d4e170ca76620b889f80655b1f6b3ef6b44c38d4
TKuang <31107156+TKuang@users.noreply.github.com>
2018-04-18 11:32:00 -0700

    Update FormulaCell.java

--- FormulaCell.java
+++ FormulaCell.java
@@ -1,5 +1,9 @@
-package textExcel;	
+package textExcel;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
 public class FormulaCell extends RealCell{
 	private Spreadsheet grid;
 	public FormulaCell(String input, Spreadsheet grid) {
@@ -11,7 +15,8 @@ public class FormulaCell extends RealCell{
 		String display = getDoubleValue() + "";
 		if(display.length() < 10) {
 			return spaces(display);
-		}else {
+		}
+		else {
 			return display.substring(0, 10);
 		}
 		
@@ -24,6 +29,7 @@ public class FormulaCell extends RealCell{
 	public double getDoubleValue() {
 		String formula = getRealCell().substring(2, getRealCell().length()-2);
 		String[] arr = formula.split(" ");
+		double[] calc = new double[(arr.length-1)/2];
 		double result = 0;
 		if(arr[0].toLowerCase().equals("avg")){
 			result = avg(arr[1].toLowerCase());
@@ -32,30 +38,32 @@ public class FormulaCell extends RealCell{
 			result = avg(arr[1].toLowerCase());
 		}
 		else {
-			for (int i = 0; i < arr.length; i++){
-				if(!Character.isDigit(arr[i].charAt(0))){
-					SpreadsheetLocation location = new SpreadsheetLocation(arr[i]);
+			for (int i = 0; i < arr.length; i+=2){
+				if(!(Character.isDigit(arr[i].charAt(0))) && !(arr[i].charAt(0) == '-')){
+					SpreadsheetLocation location = new SpreadsheetLocation(arr[0]);
 					RealCell stored = (RealCell) grid.getCell(location);
-					arr[i] = stored.getDoubleValue() + "";
+					calc[i] = stored.getDoubleValue();
+				}
+				else {
+					calc[i] = Double.parseDouble(arr[i]);
 				}
 			}
-			result = Double.parseDouble(arr[0]);
-			if(arr.length != 1) {
-				for(int i = 1; i < arr.length; i+2) {
-					if(arr[i].equals("+")){
-						result += Double.parseDouble(arr[i+1]);
-					}
-					else if(arr[i].equals("-")) {
-						result -= Double.parseDouble(arr[i+1]);
-					}
-					else if(arr[i].equals("*")){
-						result *= Double.parseDouble(arr[i+1]);
-					}
-					else if(arr[i].equals("/")){
-						result /= Double.parseDouble(arr[i+1]);
-					}	
+			result = calc[0];
+			for(int i = 1; i < arr.length; i+=2) {
+				if(arr[i].equals("+")){
+					result += calc[i];
 				}
+				else if(arr[i].equals("-")) {
+					result -= calc[i];
+				}
+				else if(arr[i].equals("*")){
+					result *= calc[i];
+				}
+				else if(arr[i].equals("/")){
+					result /= calc[i];
+				}	
 			}
+		}
 		
 		return result;
 	}
@@ -89,4 +97,97 @@ public class FormulaCell extends RealCell{
 			double avg = sum/(row * col);
 			return avg;	
 		}	
+}
+*/
+
+public class FormulaCell extends RealCell{
+	private Spreadsheet grid;
+	public FormulaCell(String input, Spreadsheet grid) {
+		super(input);
+		this.grid = grid;
+	}
+	
+	public String abbreviatedCellText() {
+		String display = getDoubleValue() + "";
+		if(display.length() < 10) {
+			return spaces(display);
+		}else {
+			return display.substring(0, 10);
+		}
+		
+	}
+
+	public String fullCellText() {
+		return super.getRealCell();
+	}
+	
+	public double getDoubleValue() {
+		String modified = getRealCell().substring(2, getRealCell().length()-2);
+		String[] arr = modified.split(" ");
+		ArrayList<String> list = new ArrayList<String>(Arrays.asList(arr));
+		double[] calc = new double[(arr.length-1)/2];
+		double result = 0;
+		if(arr[0].toLowerCase().equals("sum")) {
+			result = sum(arr[1].toLowerCase());
+		}else if(arr[0].toLowerCase().equals("avg")) {
+			result = avg(arr[1].toLowerCase());
+	    }else{
+	    	for(int i = 0; i < list.size(); i++) {
+	    			if((!(Character.isDigit(list.get(i).charAt(0))) && !(list.get(i).charAt(0) == '-'))){
+	    				SpreadsheetLocation location = new SpreadsheetLocation(list.get(i));
+	    				RealCell stored = (RealCell) grid.getCell(location);
+	    				calc[i] = stored.getDoubleValue();
+	    				list.remove(i);
+	    			}
+	    			if(Character.isDigit(list.get(i).charAt(0))){
+	    				calc[i] = Double.parseDouble(list.get(i));
+	    				list.remove(i);
+	    			}
+	    	}
+				result = calc[0];
+			for(int i = 0; i < list.size(); i++) {
+				if(list.get(i).equals("+")){
+					result += calc[i];
+				}else if(arr[i].equals("-")) {
+					result -= calc[i];
+				}else if(arr[i].equals("*")){
+					result *= calc[i];
+				}else if(arr[i].equals("/")){
+					result /= calc[i];
+				}
+			}
+		}
+		return result;
+	}
+	
+	public double sum(String expression) {//a1-c10
+		String[] operands = expression.toLowerCase().split("-");
+		int startNum = Integer.parseInt(operands[0].substring(1));
+		int endNum = Integer.parseInt(operands[1].substring(1));
+		char startChar = operands[0].charAt(0);
+		char endChar = operands[1].charAt(0);
+		double sum = 0;
+		for(char i = startChar; i <= endChar; i++) {
+			for(int j = startNum; j <= endNum; j++) {
+				SpreadsheetLocation loc = new SpreadsheetLocation("" + i + j); 
+				if(grid.getCell(loc) instanceof RealCell) {
+					RealCell temp = (RealCell)(grid.getCell(loc));
+					sum += temp.getDoubleValue();
+				}
+			}
+		}
+		return sum;
+	}
+	
+	public double avg(String expression) {
+		String[] operands = expression.split("-");
+		int startNum = Integer.parseInt(operands[0].substring(1));
+		int endNum = Integer.parseInt(operands[1].substring(1));
+		char startChar = operands[0].charAt(0);
+		char endChar = operands[1].charAt(0);
+		double s = sum(expression);
+		int totalCol = (endChar - startChar) + 1;
+		int totalRow = (endNum - startNum) + 1;
+		return s/(totalCol * totalRow);
+	}
 }
