a8b17ab125efe7cc6c539992f3585e52b28e1727
TKuang <31107156+TKuang@users.noreply.github.com>
2018-05-19 11:39:35 -0700

    Update Spreadsheet.java

--- Spreadsheet.java
+++ Spreadsheet.java
@@ -17,28 +17,36 @@ public class Spreadsheet implements Grid
 				SpreadsheetLocation location = new SpreadsheetLocation(content[1]);
 				grid[location.getRow()][location.getCol()] = new EmptyCell();
 				}
+				//clears single cell if user input specifies cell to clear
 			else {
 				clear();
+				//clears every cell, constructs empty grid
 			}
 		}
 		else if(content.length == 1) {	
 			SpreadsheetLocation location = new SpreadsheetLocation(content[0]);
 			return getCell(location).fullCellText();
+			//returns cell content if user input only asks for cell
 		}
 		else if(content.length == 3){
+		//if user input is in the formate "cell" = "value"
 			String stored = content[2];
 			SpreadsheetLocation location = new SpreadsheetLocation(content[0]);
 			if(stored.contains("\"")) {
 				grid[location.getRow()][location.getCol()] = new TextCell(stored);
+				//constructs new text cell
 			}
 			else if(stored.contains("%")) {
 				grid[location.getRow()][location.getCol()] = new PercentCell(stored);
+				//constructs new percent cell
 			}
 			else if(stored.contains("(")) {
 				grid[location.getRow()][location.getCol()] = new FormulaCell(stored, this);
+				//constructs new formula cell
 			}
 			else {
 				grid[location.getRow()][location.getCol()] = new ValueCell(stored);
+				//constructs new value cell
 			}
 		}
 		else {
