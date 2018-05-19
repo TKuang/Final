636f0edf4d2d826acddcb270e3a48c5811e32d1b
TKuang <31107156+TKuang@users.noreply.github.com>
2018-05-19 11:43:14 -0700

    Update SpreadsheetLocation.java

--- SpreadsheetLocation.java
+++ SpreadsheetLocation.java
@@ -2,25 +2,23 @@ package textExcel;
 
 public class SpreadsheetLocation implements Location
 {
-	private String cell = "";
+    private String cell = "";
     public SpreadsheetLocation(String cell) {
     	this.cell = cell;
     }
 	
-	@Override
+    @Override
     public int getRow()
     {
         // TODO Auto-generated method stub
-		int rowNum = Integer.parseInt(cell.substring(1));
-        return rowNum - 1;
+        return Integer.parseInt(cell.substring(1)) - 1;
     }
 
     @Override
     public int getCol()
     {
         // TODO Auto-generated method stub
-    	char colLetter = Character.toLowerCase(cell.charAt(0));
-        return ((int)colLetter - 'a');
+        return (int)Character.toLowerCase(cell.charAt(0)) - 'a';
     }
 
 }
