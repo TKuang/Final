c36f0b2d3d520b5dee0db798096d56693d0e66af
TKuang <31107156+TKuang@users.noreply.github.com>
2018-05-22 08:27:03 -0700

    Update PercentCell.java

--- PercentCell.java
+++ PercentCell.java
@@ -8,16 +8,20 @@ public class PercentCell extends RealCell{
 	
 	public String abbreviatedCellText() {
 		String revised = super.getRealCell().substring(0, super.getRealCell().indexOf('.'));
+		//stores whole number of the percentage as it is (this method doesn't round the value)
 		return super.spaces(revised + "%");
+		//adds a percent sign to stored percentage value and adds spaces to it until it reaches length of 10
 	}
 	
 	public String fullCellText() {
 		return getDoubleValue() * 0.01 + "";
+		//returns the percentage in decimal format (out of 1 instead of 100)
 	}
 	
 	public double getDoubleValue() {
 		String percent = super.getRealCell();
 		return Double.parseDouble(percent);
+		//returns percentage as a double
 	}
 		
 }
