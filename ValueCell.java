ee2f9b796fa63a0f898eb80f7d0185c68862bda8
TKuang <31107156+TKuang@users.noreply.github.com>
2018-05-22 08:21:26 -0700

    Update ValueCell.java

--- ValueCell.java
+++ ValueCell.java
@@ -10,8 +10,10 @@ public class ValueCell extends RealCell{
 		if(revised.length() < 10) {
 			if(!revised.contains(".")) {    
 				revised += ".0";
+				//adds ".0" to the string if it doesn't contain a decimal
 			}
 			return super.spaces(revised);
+			//adds spaces to string until it reaches the length of 20
 		}
 		else {	
 			return revised.substring(0,10);
@@ -24,6 +26,7 @@ public class ValueCell extends RealCell{
 	
 	public double getDoubleValue() {
 		return Double.parseDouble(getRealCell());
+		//casts real cell in a double
 	}
 	
 }
